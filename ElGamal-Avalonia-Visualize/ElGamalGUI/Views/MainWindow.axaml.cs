26d4c7c103e2adbafe4ff07da9dbc2c65671306f
AnastasiaZAYU <anastasiya7.0312@gmail.com>
2026-03-03 10:26:56 +0200

    feat: initial Avalonia UI project structure for ElGamal visualizer

---
+++ ElGamal-Avalonia-Visualize/ElGamalGUI/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace ElGamalGUI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}

4d6d69a8a109a5674b6fb4656b8fa1770a46c60a
AnastasiaZAYU <anastasiya7.0312@gmail.com>
2026-03-03 11:14:20 +0200

    refactor: setup ViewModel with properties and commands

--- ElGamal-Avalonia-Visualize/ElGamalGUI/ViewModels/MainWindowViewModel.cs
+++ ElGamal-Avalonia-Visualize/ElGamalGUI/ViewModels/MainWindowViewModel.cs
@@ -1,7 +1,44 @@
-﻿namespace ElGamalGUI.ViewModels
+﻿using System.Threading.Tasks;
+using System.Reactive;
+using ReactiveUI;
+using ElGamalGUI.Models;
+
+namespace ElGamalGUI.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
 {
-    public partial class MainWindowViewModel : ViewModelBase
+    private string _pText = "Not generated";
+    private string _gText = "Not generated";
+
+    public string PText
     {
-        public string Greeting { get; } = "Welcome to Avalonia!";
+        get => _pText;
+        set => SetProperty(ref _pText, value);
+    }
+
+    public string GText
+    {
+        get => _gText;
+        set => SetProperty(ref _gText, value);
+    }
+
+    public ReactiveCommand<Unit, Unit> GenerateKeysCommand { get; }
+
+    public MainWindowViewModel()
+    {
+        GenerateKeysCommand = ReactiveCommand.CreateFromTask(GenerateKeysAsync);
+    }
+
+    private async Task GenerateKeysAsync()
+    {
+        var service = new ElGamalService();
+
+        await Task.Run(() =>
+        {
+            var parameters = service.GenerateParameters(3072);
+            PText = parameters.P.ToString();
+            GText = parameters.G.ToString();
+        });
     }
 }
+
